6573f154a3911ecaa937c3643eefd94d9b6da84f
newsaeta <isaacpda@yahoo.es>
2017-07-18 00:24:39 +0200

    ultimo commit del dia

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,15 @@
 var gulp = require("gulp");
 var sass = require("gulp-sass");
+var notify = require("gulp-notify");
 var browserSync = require("browser-sync").create();
+
 
 //definicmos la tarea por defecto
 gulp.task("default", function(){
     // iniciamos el servidor de desarrollo
     browserSync.init({ server: "src/"});
-    
+    //proxy: "http://127.0.0.0:8080" para otro backend cambiamos server por proxy
+
     // observa cambios en los archivos SASS y ejecuta las tarea compile-sass
     gulp.watch(["src/scss/*.scss", "src/scss/**/*.scss"], ["compile-sass"]);
     
@@ -20,5 +23,6 @@ gulp.task("compile-sass", function(){
         .pipe(sass().on("error", sass.logError)) //lo compilamos con gulp-sass
         .pipe(gulp.dest("src/css")) // lo guardamos en la carpeta css
         .pipe(browserSync.stream()) // recarga el css del navegador
+        .pipe(notify("SASS Compilado 😎 ")) //muestra notificacion en pantalla
 
 });
