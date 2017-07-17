328c579ee598d0c22f5e1a908e8e3432c891ea5c
newsaeta <isaacpda@yahoo.es>
2017-07-17 22:13:31 +0200

    Second commit

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+var gulp = require("gulp");
+
+//definicmos la tarea por defecto
+gulp.task("default", function(){
+    console.log("Hello world");
+})
+
+//compilar sass
+gulp.task("sass", function(){
+    console.log("compilando sas");
+})
