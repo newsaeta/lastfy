61092c113e6e022c87ce383cb9f5027ef1065bff
newsaeta <isaacpda@yahoo.es>
2017-07-18 00:07:24 +0200

    gulp recargando html y css

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,24 @@
 var gulp = require("gulp");
+var sass = require("gulp-sass");
+var browserSync = require("browser-sync").create();
 
 //definicmos la tarea por defecto
 gulp.task("default", function(){
-    console.log("Hello world");
+    // iniciamos el servidor de desarrollo
+    browserSync.init({ server: "src/"});
+    
+    // observa cambios en los archivos SASS y ejecuta las tarea compile-sass
+    gulp.watch(["src/scss/*.scss", "src/scss/**/*.scss"], ["compile-sass"]);
+    
+    // observa los cambios en los archivos html y recarga el navegador
+    gulp.watch("src/*.html").on("change", browserSync.reload);
 })
 
 //compilar sass
-gulp.task("sass", function(){
-    console.log("compilando sas");
-})
+gulp.task("compile-sass", function(){
+    gulp.src("./src/scss/style.scss") //cargamos el archivo styles.scsss
+        .pipe(sass().on("error", sass.logError)) //lo compilamos con gulp-sass
+        .pipe(gulp.dest("src/css")) // lo guardamos en la carpeta css
+        .pipe(browserSync.stream()) // recarga el css del navegador
+
+});
